d56f7ad65053aaba6f69c53b5c2145b727db4f03
Nathan Walters <nwalters512@gmail.com>
2021-01-05 21:10:04 -0800

    Wire up collapsing navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import classnames from "classnames";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./Header.module.scss";
 import NavLink from "./NavLink";
 
 export const Header: React.FC = () => {
+  const [collapsed, setCollapsed] = React.useState(true);
+
+  // Collapse the nav when the user navigates to a different page
+  const { pathname } = useRouter();
+  React.useEffect(() => {
+    setCollapsed(true);
+  }, [pathname]);
+
   return (
     <nav
       className={classnames(
@@ -17,7 +26,20 @@ export const Header: React.FC = () => {
         <Link href="/">
           <a className="navbar-brand">PrairieLearn</a>
         </Link>
-        <div className="collapse navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          aria-controls="navbar"
+          aria-expanded={collapsed ? "false" : "true"}
+          aria-label="Toggle navigation"
+          onClick={() => setCollapsed((collapsed) => !collapsed)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={classnames("navbar-collapse", { collapse: collapsed })}
+          id="navbar"
+        >
           <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
             <li className="nav-item">
               <NavLink href="/gallery">Gallery</NavLink>
