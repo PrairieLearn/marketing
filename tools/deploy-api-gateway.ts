699686349693e5aa1b6cc76bee79ab1d4936daa7
Nathan Walters <nwalters512@gmail.com>
2021-03-27 18:44:59 -0700

    Update API Gateway deployment script

--- tools/deploy-api-gateway.ts
+++ tools/deploy-api-gateway.ts
@@ -2,10 +2,15 @@ import AWS from "aws-sdk";
 import { getApiRoutes, getImageNameForRoute } from "./api-utils";
 import { AWS_REGION, API_GATEWAY_ID, getAwsAccountId } from "./aws";
 
+const OPTIONAL_CATCH_ALL_REGEX = /^\[\[\.\.\.(\w+)\]\]$/;
+const CATCH_ALL_REGEX = /^\[\.\.\.(\w+)\]$/;
+const DYNAMIC_REGEX = /^\[(\w+)\]$/;
+
 const createIntegration = async (
   apiGateway: AWS.ApiGatewayV2,
   lambdaArn: string
 ) => {
+  console.log(`Creating integration for ${lambdaArn}`);
   const integration = await apiGateway
     .createIntegration({
       ApiId: API_GATEWAY_ID,
@@ -15,6 +20,46 @@ const createIntegration = async (
     .promise();
 
   return { lambdaArn, integrationId: integration.IntegrationId };
+};
+
+const createRoute = async (
+  apiGateway: AWS.ApiGatewayV2,
+  routeKey: string,
+  integrationId: string
+) => {
+  console.log(`Creating route`);
+  const route = await apiGateway
+    .createRoute({
+      ApiId: API_GATEWAY_ID,
+      RouteKey: routeKey,
+      Target: `integrations/${integrationId}`,
+    })
+    .promise();
+
+  return { routeKey, routeId: route.RouteId };
+};
+
+const deleteIntegration = async (
+  apiGateway: AWS.ApiGatewayV2,
+  integrationId: string
+) => {
+  console.log(`Deleting integration ${integrationId}`);
+  await apiGateway
+    .deleteIntegration({
+      ApiId: API_GATEWAY_ID,
+      IntegrationId: integrationId,
+    })
+    .promise();
+};
+
+const deleteRoute = async (apiGateway: AWS.ApiGatewayV2, routeId: string) => {
+  console.log(`Deleting route ${routeId}`);
+  await apiGateway
+    .deleteRoute({
+      ApiId: API_GATEWAY_ID,
+      RouteId: routeId,
+    })
+    .promise();
 };
 
 export const deployApiGateway = async () => {
@@ -57,8 +102,118 @@ export const deployApiGateway = async () => {
     )
   );
 
-  console.log(routes);
-  console.log(integrations);
+  // Create a combined list of integrations for the next phase
+  // This maps Lambda ARNs to the corresponding integration ID
+  const allIntegrations = {
+    ...integrations.Items?.reduce(
+      (acc, integration) => ({
+        ...acc,
+        [integration.IntegrationUri || "NONE"]: integration.IntegrationId,
+      }),
+      {} as Record<string, string | undefined>
+    ),
+    ...newIntegrations.reduce(
+      (acc, integration) => ({
+        ...acc,
+        [integration.lambdaArn]: integration.integrationId,
+      }),
+      {} as Record<string, string | undefined>
+    ),
+  };
+
+  // Build a list of all the routes we'll need
+  // Starting with Next.js route syntax, we'll convert it into what API Gateway expects
+  const neededRoutes = Object.keys(apiRoutes).map((routePath) => {
+    const transformedRoute = routePath
+      .split("/")
+      .map((pathSegment) => {
+        if (OPTIONAL_CATCH_ALL_REGEX.test(pathSegment)) {
+          const segmentName = OPTIONAL_CATCH_ALL_REGEX.exec(pathSegment)?.[1];
+          return `{${segmentName}+}`;
+        } else if (CATCH_ALL_REGEX.test(pathSegment)) {
+          const segmentName = CATCH_ALL_REGEX.exec(pathSegment)?.[1];
+          return `{${segmentName}+}`;
+        } else if (DYNAMIC_REGEX.test(pathSegment)) {
+          const segmentName = DYNAMIC_REGEX.exec(pathSegment)?.[1];
+          return `{${segmentName}}`;
+        }
+        return pathSegment;
+      })
+      .join("/");
+
+    return {
+      routePath,
+      routeKey: `ANY ${transformedRoute}`,
+    };
+  });
+
+  const missingRoutes = neededRoutes.filter(
+    ({ routeKey }) =>
+      !routes.Items?.some(({ RouteKey }) => RouteKey === routeKey)
+  );
+
+  const newRoutes = await Promise.all(
+    missingRoutes.map(async ({ routeKey, routePath }) => {
+      // Map the route path to a lambda ARN
+      const lambdaArn = neededIntegrations[routePath];
+
+      // Map the lambda ARN to an integration
+      const integrationId = allIntegrations[lambdaArn];
+      if (!integrationId) {
+        throw new Error(`Could not determine integration ID for ${lambdaArn}`);
+      }
+
+      // Create a new route with the route key and integration
+      return await createRoute(apiGateway, routeKey, integrationId);
+    })
+  );
+
+  // Build a set of all route IDs for the next step
+  const allRouteKeys = [
+    ...(routes.Items?.map((route) => route.RouteId) ?? []),
+    ...newRoutes.map((route) => route.routeId),
+  ];
+
+  // Now we need to clean up after ourselves, i.e. remove all unused routes
+  // and integrations. All newly-created routes are guaranteed to be valid,
+  // so we just have to consider the original set of routes and integrations
+  // and remove them.
+  const allUsedIntegrationUris = Object.values(neededIntegrations);
+  const allUsedRouteKeys = neededRoutes.map(({ routeKey }) => routeKey);
+  const unusedIntegrations =
+    integrations.Items?.filter(
+      (integration) =>
+        !allUsedIntegrationUris.some(
+          (integrationUri) => integration.IntegrationUri === integrationUri
+        )
+    ) ?? [];
+  const unusedRoutes =
+    routes.Items?.filter(
+      (route) =>
+        !allUsedRouteKeys.some((routeKey) => route.RouteKey === routeKey)
+    ) ?? [];
+
+  if (unusedIntegrations.length > 0) {
+    console.log("Deleting unused integration");
+    await Promise.all(
+      unusedIntegrations.map(async ({ IntegrationId }) => {
+        if (IntegrationId) {
+          await deleteIntegration(apiGateway, IntegrationId);
+        }
+      })
+    );
+  }
+
+  if (unusedRoutes.length > 0) {
+    console.log("Deleting unused routes");
+    await Promise.all(
+      unusedRoutes.map(async ({ RouteId }) => {
+        if (RouteId) {
+          await deleteRoute(apiGateway, RouteId);
+        }
+      })
+    );
+  }
 };
 
 if (require.main === module) {
