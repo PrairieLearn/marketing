384303565c4a52b88aaf18f3d26526ec9921f4cc
Mariana <mfsilva@illinois.edu>
2021-11-12 16:28:07 -0800

    fixed typeScript error

--- src/components/ButtonToPage.tsx
+++ src/components/ButtonToPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Link from "next/link";
 
-export const ButtonToPage: React.FC = ({ text, page }) => (
+export interface ButtonToPageProps {
+  text: string;
+  page: string;
+}
+
+export const ButtonToPage: React.FC<ButtonToPageProps> = ({ text, page }) => (
   <div className="row justify-content-center my-2">
     <div className="col-md-12 text-center py-3">
-      <Link href={ page }>
-        <a className="btn btn-warning btn-md">{ text }</a>
+      <Link href={page}>
+        <a className="btn btn-warning btn-md">{text}</a>
       </Link>
     </div>
   </div>
