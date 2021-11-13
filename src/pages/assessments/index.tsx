356415f0b11e29dc3f348de214fc07854e38fa26
Mariana <mfsilva@illinois.edu>
2021-11-12 17:13:09 -0800

    added blank assessment page

---
+++ src/pages/assessments/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+import { PageBanner } from "../../components/Banner";
+
+
+export default function Assessments() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Assessments | PrairieLearn</title>
+      </Head>
+      <PageBanner
+        title="Assessments"
+        text="Building different types of activities for your class"
+      />
+    </React.Fragment>
+  );
+}
