1756f4a2fe56fc271566e28b9a611f6af3ed9273
Mariana Silva <mfsilva22@users.noreply.github.com>
2022-01-24 11:45:57 -0600

    Update src/pages/gallery/index.tsx
    Co-authored-by: Nathan Walters <nathan@prairielearn.com>

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -93,7 +93,7 @@ const GalleryIndex: React.FC<GalleryIndexProps> = ({ questions }) => {
               <p>
                 <strong> Write it once, use many times!</strong> Since questions
                 are defined as code, they can be reused in many future
-                assessment.s And students can keep trying new variants of
+                assessments. And students can keep trying new variants of
                 difficult problems until they&apos;ve mastered the topic—no need
                 for you to manually write new questions.
               </p>
