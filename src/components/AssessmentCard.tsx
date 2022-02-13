34254d00143b2d2f0163ff775642ec4965d17eb1
Mariana Silva <mfsilva@illinois.edu>
2022-02-13 16:26:29 -0600

    fixed buttons to bottom assessment card

--- src/components/AssessmentCard.tsx
+++ src/components/AssessmentCard.tsx
@@ -14,10 +14,10 @@ export const AssessmentCard: React.FC<AssessmentCardProps> = ({
   body,
 }) => (
   <div className="card">
-    <div className="card-body">
+    <div className="card-body d-flex flex-column">
       <h3 className="card-title">{title}</h3>
       <p className="card-text">{body}</p>
-      <div className="row justify-content-center">
+      <div className="row justify-content-center mt-auto">
         <div className="col-md-4  col-sm-4 col-xs-2 py-1 px-1">
           <LinkButton label="Read more" href={readmoreHref} />
         </div>
