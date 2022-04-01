51b29b57edd2e08ad3f0b76a2ad15f4b6d184e18
Nathan Walters <nathan@prairielearn.com>
2022-04-01 16:30:11 -0700

    Revert changes to Heading component

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,13 +3,6 @@ import classnames from "classnames";
 
 import styles from "./Heading.module.scss";
 
-interface HeadingProps {
-  children: React.ReactNode;
-  className?: string;
-}
-
-export const Heading: React.FC<HeadingProps> = ({ children, className }) => (
-  <h2 className={classnames(styles.heading, "mb-3 h2", className)}>
-    {children}
-  </h2>
+export const Heading: React.FC = ({ children }) => (
+  <h2 className={classnames(styles.heading, "mb-3 h2")}>{children}</h2>
 );
