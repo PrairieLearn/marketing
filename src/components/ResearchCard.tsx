ec075ef53bf05d1436750fbaf481c5999d2c4e8d
Mariana Silva <mfsilva@illinois.edu>
2022-04-01 16:40:09 -0500

    adding content to research page - wip

--- src/components/ResearchCard.tsx
+++ src/components/ResearchCard.tsx
@@ -1,35 +1,28 @@
 import React from "react";
-import { LinkButton } from "./LinkButton";
+import { Link } from "./Link";
+
+import classnames from "classnames";
+import styles from "./ResearchCard.module.scss";
 
 export interface ResearchCardProps {
-  // assessmentHref: string;
-  // readmoreHref: string;
   title: string;
   body: string;
+  paperHref: string;
+  reference: string;
 }
 export const ResearchCard: React.FC<ResearchCardProps> = ({
-  // assessmentHref,
-  // readmoreHref,
   title,
   body,
+  paperHref,
+  reference,
 }) => (
-  <div className="card">
+  <div className="card w-75 mx-auto">
     <div className="card-body d-flex flex-column">
-    <div className="col-md-6 order-2 pt-4">
-              <Image src={assessmentImage} alt="assessment page view" />
-            </div>
-            <div className="col-md-6 order-1">
-              
       <h3 className="card-title">{title}</h3>
       <p className="card-text">{body}</p>
-      {/* <div className="row justify-content-center mt-auto">
-        <div className="col-md-4  col-sm-4 col-xs-2 py-1 px-1">
-          <LinkButton label="Read more" href={readmoreHref} />
-        </div>
-        <div className="col-md-4 col-sm-4 col-xs-2 py-1 px-1">
-          <LinkButton label="Try it now!" href={assessmentHref} />
-        </div>
-      </div> */}
+      <Link href={paperHref}>
+        <p className={classnames(styles["card-text"], "card-text")}>{reference}</p>
+      </Link>
     </div>
   </div>
 );
