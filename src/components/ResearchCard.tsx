745f24a46a1ba5ddede8c167c69b005804fd1fc1
Nathan Walters <nathan@prairielearn.com>
2022-04-04 12:54:30 -0700

    Adjust ResearchCard component implementation

--- src/components/ResearchCard.tsx
+++ src/components/ResearchCard.tsx
@@ -3,21 +3,21 @@ import { Link } from "./Link";
 
 export interface ResearchCardProps {
   title: string;
-  body: string;
-  paperHref: string;
+  children: React.ReactNode;
   reference: string;
+  referenceHref: string;
 }
 export const ResearchCard: React.FC<ResearchCardProps> = ({
   title,
-  body,
-  paperHref,
+  children,
+  referenceHref,
   reference,
 }) => (
   <div className="card">
     <div className="card-body d-flex flex-column">
       <h3 className="card-title">{title}</h3>
-      <p className="card-text">{body}</p>
-      <Link href={paperHref} className="small">
+      <p className="card-text">{children}</p>
+      <Link href={referenceHref} className="small">
         {reference}
       </Link>
     </div>
