98f4319955250a8925e4df2c564cfcdc4e26128a
Nathan Walters <nathan@prairielearn.com>
2022-06-03 10:39:38 -0700

    Rework footer markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,9 @@ interface FooterLinkProps {
 
 const FooterLink: React.FC<FooterLinkProps> = ({ href, children }) => {
   return (
-    <p className="my-0">
-      <a href={href} className="link-secondary text-decoration-none">
-        {children}
-      </a>
-    </p>
+    <a href={href} className="link-secondary text-decoration-none">
+      {children}
+    </a>
   );
 };
 
@@ -20,27 +18,47 @@ export const Footer: React.FC = () => {
     <footer className="bg-light pt-4">
       <div className="container-md">
         <div className="row">
-          <div className="col-12 col-sm-6 col-md-4 mb-4">
+          <div className="col-6 col-sm-4 mb-3">
             <h6 className="text-uppercase text-secondary fw-bold">Resources</h6>
-            <FooterLink href="https://prairielearn.readthedocs.io/en/latest/">
-              Documentation
-            </FooterLink>
-            <FooterLink href="https://github.com/PrairieLearn/PrairieLearn">
-              GitHub
-            </FooterLink>
-            <FooterLink href="/research">Research</FooterLink>
-            <FooterLink href="/pricing">Pricing</FooterLink>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <FooterLink href="https://prairielearn.readthedocs.io/en/latest/">
+                  Documentation
+                </FooterLink>
+              </li>
+              <li className="mb-2">
+                <FooterLink href="https://github.com/PrairieLearn/PrairieLearn">
+                  GitHub
+                </FooterLink>
+              </li>
+              <li className="mb-2">
+                <FooterLink href="/research">Research</FooterLink>
+              </li>
+              <li className="mb-2">
+                <FooterLink href="/pricing">Pricing</FooterLink>
+              </li>
+            </ul>
           </div>
-          <div className="col-12 col-sm-6 col-md-4 mb-4">
+          <div className="col-6 col-sm-4 mb-3">
             <h6 className="text-uppercase text-secondary fw-bold">Legal</h6>
-            <FooterLink href="/legal/terms">Terms of Service</FooterLink>
-            <FooterLink href="/legal/privacy">Privacy Policy</FooterLink>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <FooterLink href="/legal/terms">Terms of Service</FooterLink>
+              </li>
+              <li className="mb-2">
+                <FooterLink href="/legal/privacy">Privacy Policy</FooterLink>
+              </li>
+            </ul>
           </div>
-          <div className="col-12 col-sm-6 col-md-4 mb-4">
+          <div className="col-6 col-sm-4 mb-3">
             <h6 className="text-uppercase text-secondary fw-bold">Contact</h6>
-            <FooterLink href="mailto:hello@prairielearn.com">
-              hello@prairielearn.com
-            </FooterLink>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <FooterLink href="mailto:hello@prairielearn.com">
+                  hello@prairielearn.com
+                </FooterLink>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
