80b04eaab5704bc7a6f81afc741a331be387e441
Nathan Walters <nathan@prairielearn.com>
2022-06-10 11:42:58 -0700

    Use cards; add images

--- src/pages/gallery/courses/index.tsx
+++ src/pages/gallery/courses/index.tsx
@@ -1,10 +1,65 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { ImageProps } from "next/image";
 import classnames from "classnames";
 
 import { PageBanner } from "../../../components/Banner";
 import { Heading } from "../../../components/Heading";
+import Image from "../../../components/Image";
+
+import styles from "./index.module.scss";
+import cs233Image from "../../../lib/images/cs-233.png";
+
+interface CourseCardProps {
+  image: ImageProps["src"];
+  title: string;
+  href: string;
+  institution: string;
+  rubric: string;
+  ownerName: string;
+  ownerEmail: string;
+  children: React.ReactNode;
+}
+
+const CourseCard: React.FC<CourseCardProps> = ({
+  image,
+  title,
+  href,
+  institution,
+  rubric,
+  ownerName,
+  ownerEmail,
+  children,
+}) => {
+  return (
+    <article className="card">
+      <Link href={href}>
+        <a className="position-relative" style={{ aspectRatio: "4 / 3" }}>
+          <Image src={image} layout="fill" objectFit="contain" alt={title} />
+        </a>
+      </Link>
+      <div className="card-body">
+        <Link href={href}>
+          <a>
+            <h3 className="card-title h5">{title}</h3>
+          </a>
+        </Link>
+        <p className="mb-1">
+          <strong>Institution:</strong> {institution}
+        </p>
+        <p className="mb-1">
+          <strong>Rubric: </strong> {rubric}
+        </p>
+        <p className="mb-1">
+          <strong>Instructor:</strong> {ownerName} (
+          <a href={`mailto:${ownerEmail}`}>{ownerEmail}</a>)
+        </p>
+        {children}
+      </div>
+    </article>
+  );
+};
 
 export default function Courses() {
   return (
@@ -27,29 +82,57 @@ export default function Courses() {
             <Link href="/gallery/questions">questions</Link> you have created.
           </p>
 
-          <h3 className="h4">
-            <a href="https://www.prairielearn.org/pl/course_instance/128979">
-              Computer Architecture
-            </a>
-          </h3>
-          <p className="mb-1">
-            <strong>Institution:</strong> University of Illinois
-            Urbana-Champaign
-          </p>
-          <p className="mb-1">
-            <strong>Rubric: </strong> CS 233
-          </p>
-          <p className="mb-1">
-            <strong>Instructor:</strong> Geoffrey Herman (
-            <a href="mailto:glherman@Illinois.edu">glherman@Illinois.edu</a>)
-          </p>
-          <p>
-            This course teaches the fundamentals of computer architecture,
-            including digital logic design, machine-level programming, and
-            performance models of modern computer hardware. Its PrairieLearn
-            questions include a variety of novel problem types, including
-            randomly-generated state machines, ALUs, truth tables, and more.
-          </p>
+          <div className={styles.grid}>
+            <CourseCard
+              image={cs233Image}
+              title="Computer Architecture"
+              href="https://www.prairielearn.org/pl/course_instance/128979"
+              institution="University of Illinois Urbana-Champaign"
+              rubric="CS 233"
+              ownerName="Geoffrey Herman"
+              ownerEmail="glherman@illinois.edu"
+            >
+              <p className="mb-0">
+                This course teaches the fundamentals of computer architecture,
+                including digital logic design, machine-level programming, and
+                performance models of modern computer hardware. Its PrairieLearn
+                questions include a variety of novel problem types, including
+                randomly-generated state machines, ALUs, truth tables, and more.
+              </p>
+            </CourseCard>
+
+            <CourseCard
+              image={cs233Image}
+              title="Computer Architecture"
+              href="https://www.prairielearn.org/pl/course_instance/128979"
+              institution="University of Illinois Urbana-Champaign"
+              rubric="CS 233"
+              ownerName="Geoffrey Herman"
+              ownerEmail="glherman@illinois.edu"
+            >
+              <p className="mb-0">
+                This is some different text to test card sizes.
+              </p>
+            </CourseCard>
+
+            <CourseCard
+              image={cs233Image}
+              title="Computer Architecture"
+              href="https://www.prairielearn.org/pl/course_instance/128979"
+              institution="University of Illinois Urbana-Champaign"
+              rubric="CS 233"
+              ownerName="Geoffrey Herman"
+              ownerEmail="glherman@illinois.edu"
+            >
+              <p className="mb-0">
+                This course teaches the fundamentals of computer architecture,
+                including digital logic design, machine-level programming, and
+                performance models of modern computer hardware. Its PrairieLearn
+                questions include a variety of novel problem types, including
+                randomly-generated state machines, ALUs, truth tables, and more.
+              </p>
+            </CourseCard>
+          </div>
         </div>
       </div>
     </React.Fragment>
