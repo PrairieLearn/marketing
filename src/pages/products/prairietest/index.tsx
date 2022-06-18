50cdf45fd9ec1deded35af1d178a808431ea21b0
Mariana Silva <mfsilva@illinois.edu>
2022-06-18 15:30:09 -0500

    adding pt draft page

--- src/pages/products/prairietest/index.tsx
+++ src/pages/products/prairietest/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classnames from "classnames";
 import Head from "next/head";
+import Link from "next/link";
 
 import { PageBanner } from "../../../components/Banner";
-import { DemoCourseCTA } from "../../../components/DemoCourse";
-// import { Heading } from "../../components/Heading";
-// import Stack from "../../components/Stack";
+import { Heading } from "../../../components/Heading";
+import Stack from "../../../components/Stack";
 
 import styles from "./index.module.scss";
 
@@ -13,29 +13,58 @@ export default function Testing() {
   return (
     <React.Fragment>
       <Head>
-        <title>Testing | PrairieLearn</title>
+        <title>PrairieTest | PrairieLearn</title>
       </Head>
 
-      <PageBanner title="Scaling Testing" subtitle="bla" />
+      <PageBanner title="PrairieTest" subtitle="Making the logistics and delivery of exams a painless process" />
 
-      <div className={classnames("container-fluid py-4", styles.container)}>
+      <div className={classnames("container-fluid py-4")}>
         <div className="container-md">
-          {/* <Stack spacing={5}>
+          <Stack spacing={5}>
             <Stack spacing={4}>
               <Heading>
-                Mastery learning, randomized assessments and instant feedback
+                Delivering exams at a computer-based testing facility
               </Heading>
 
+              <p>add text here; we should also add a photo</p>
+
             </Stack>
-          </Stack> */}
+          </Stack>
         </div>
       </div>
 
-      <DemoCourseCTA
-        title="View demo course!"
-        subtitle="Explore the demo course to see how this all comes together"
-        buttonLabel="Demo course"
-      />
+      <div className={classnames("container-fluid py-4")}>
+        <div className="container-md">
+          <Stack spacing={5}>
+            <Stack spacing={4}>
+              <Heading>
+                Running Bring-Your-Own-Device exams in classrooms or online
+              </Heading>
+
+              <p>add text here; we should also add a photo</p>
+
+            </Stack>
+          </Stack>
+        </div>
+      </div>
+
+      <div className={classnames("container-fluid py-4", styles.demo_container)}>
+        <div className="container-md">
+          <div className="row">
+            <div className="col">
+              <h3 className="text-white display-6">Do you want to learn more?</h3>
+            </div>
+          </div>
+          <div className="row justify-content-center my-2">
+            <div className="col-md-12 text-center">
+              <Link href="/contact">
+                <a className="btn btn-warning btn-lg">Request a demo</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </React.Fragment>
   );
 }
