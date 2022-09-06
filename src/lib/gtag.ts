df4f7c7f48f161f2a10aa63007666436e00f75e7
Nathan Walters <nathan@prairielearn.com>
2022-09-06 13:03:05 -0700

    Remove Google Analytics

--- src/lib/gtag.ts
+++
@@ -1,9 +0,0 @@
-export const GA_TRACKING_ID = "G-20ZFCK3V0P";
-
-export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID, { page_path: url });
-};
-
-export const event = (name: string, parameters: Record<string, any>) => {
-  window.gtag("event", name, parameters);
-};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,12 @@
 import React from "react";
 import { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import SSRProvider from "react-bootstrap/SSRProvider";
 
 import "../styles/bootstrap.scss";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import * as gtag from "../lib/gtag";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  React.useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <React.Fragment>
       <SSRProvider>
