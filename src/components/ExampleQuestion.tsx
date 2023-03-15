4877625802d51a3586c45ebbe9ab38c4d56de79f
Nathan Walters <nathan@prairielearn.com>
2023-03-14 22:24:01 -0400

    Remove logs

--- src/components/ExampleQuestion.tsx
+++ src/components/ExampleQuestion.tsx
@@ -7,10 +7,8 @@ import React from "react";
  */
 function mulberry32(a: number): () => number {
   return function () {
-    console.log("start a", a);
     a |= 0;
     a = (a + 0x6d2b79f5) | 0;
-    console.log("a", a);
     var t = Math.imul(a ^ (a >>> 15), 1 | a);
     t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
     return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
