f6cab4834baba700be825166b4c9d44082566f74
Mariana Silva <mfsilva@illinois.edu>
2023-09-02 19:04:27 -0500

    added preheader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import SSRProvider from "react-bootstrap/SSRProvider";
 
 import "../styles/bootstrap.scss";
+import { PreHeader } from "../components/PreHeader";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
@@ -10,6 +11,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <React.Fragment>
       <SSRProvider>
+        <PreHeader />
         <Header />
         <Component {...pageProps} />
         <Footer />
