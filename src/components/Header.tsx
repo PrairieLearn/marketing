833b4f844928022427d597314bcdaac5423edfed
Mariana Silva <mfsilva@illinois.edu>
2023-09-17 17:19:34 -0500

    removed preHeader content into Hearder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,14 @@ import classnames from "classnames";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Dropdown, NavDropdown, NavItem, NavLink } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownButton,
+  NavDropdown,
+  NavItem,
+  NavLink,
+} from "react-bootstrap";
+import { RequestCourseModal } from "../components/RequestCourseModal";
 
 import styles from "./Header.module.scss";
 
@@ -59,6 +66,9 @@ import logo from "../lib/images/PL-logo-white.svg";
 export const Header: React.FC = () => {
   const [collapsed, setCollapsed] = React.useState(true);
 
+  const [showRequestCourseModal, setShowRequestCourseModal] =
+    React.useState(false);
+
   // Collapse the nav when the user navigates to a different page
   const { pathname } = useRouter();
   React.useEffect(() => {
@@ -66,85 +76,111 @@ export const Header: React.FC = () => {
   }, [pathname]);
 
   return (
-    <nav
-      className={classnames(
-        "navbar navbar-expand-sm navbar-dark navbar-primary",
-        styles.header
-      )}
-    >
-      <div className="container">
-        <Link href="/" className="navbar-brand">
-          <Image
-            src={logo}
-            height={60}
-            alt="PrairieLearn logo"
-            style={{
-              width: "auto",
-            }}
-          />
-        </Link>
-        <div className="d-flex flex-row">
+    <React.Fragment>
+      <div className={classnames("container-fluid pt-4", styles.header)}>
+        <div className="container-md text-end">
           <button
-            className="navbar-toggler"
-            type="button"
-            aria-controls="navbar"
-            aria-expanded={collapsed ? "false" : "true"}
-            aria-label="Toggle navigation"
-            onClick={() => setCollapsed((collapsed) => !collapsed)}
+            className="btn btn-light btn-md me-3 "
+            onClick={() => setShowRequestCourseModal(true)}
           >
-            <span className="navbar-toggler-icon"></span>
+            Free sign up
           </button>
-        </div>
-        <div
-          className={classnames("navbar-collapse", { collapse: collapsed })}
-          id="navbar"
-        >
-          <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
-            <Dropdown as={NavItem}>
-              <Dropdown.Toggle
-                as={NavLink}
-                className={classnames(styles["nav-link"], {
-                  [`fw-bold ${styles.active}`]: useIsActive("/gallery"),
-                })}
-              >
-                Catalog
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <NavDropdownItem href="/gallery/questions">
-                  Questions
-                </NavDropdownItem>
-                <NavDropdownItem href="/gallery/assessments">
-                  Assessments
-                </NavDropdownItem>
-                <NavDropdownItem href="/gallery/courses">
-                  Courses
-                </NavDropdownItem>
-                <NavDropdownItem href="/oer">OER</NavDropdownItem>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown as={NavItem}>
-              <Dropdown.Toggle
-                as={NavLink}
-                className={classnames(styles["nav-link"], {
-                  [`fw-bold ${styles.active}`]: useIsActive("/about"),
-                })}
-              >
-                About
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <NavDropdownItem href="/about">About Us</NavDropdownItem>
-                <NavDropdownItem href="/research">Research</NavDropdownItem>
-              </Dropdown.Menu>
-            </Dropdown>
-            <li className="nav-item">
-              <RouterNavLink href="/contact">Support</RouterNavLink>
-            </li>
-            <li className="nav-item">
-              <RouterNavLink href="/pricing">Pricing</RouterNavLink>
-            </li>
-          </ul>
+          <DropdownButton
+            id="login-dropdown-desktop"
+            title="Login"
+            variant="light"
+            className="d-none d-sm-inline-block"
+          >
+            <Dropdown.Item href="https://us.prairielearn.com/pl/login">
+              Main <span className="text-muted">(us.prairielearn.com)</span>
+            </Dropdown.Item>
+            <Dropdown.Item href="https://ca.prairielearn.com/pl/login">
+              Canada <span className="text-muted">(ca.prairielearn.com)</span>
+            </Dropdown.Item>
+          </DropdownButton>
         </div>
       </div>
-    </nav>
+      <RequestCourseModal
+        show={showRequestCourseModal}
+        onHide={() => setShowRequestCourseModal(false)}
+      />
+      <nav
+        className={classnames(
+          "navbar navbar-expand-sm navbar-dark navbar-primary",
+          styles.header
+        )}
+      >
+        <div className="container">
+          <Link href="/" className="navbar-brand">
+            <Image src={logo} height={60} alt="PrairieLearn logo" />
+          </Link>
+          <div className="d-flex flex-row">
+            <button
+              className="navbar-toggler"
+              type="button"
+              aria-controls="navbar"
+              aria-expanded={collapsed ? "false" : "true"}
+              aria-label="Toggle navigation"
+              onClick={() => setCollapsed((collapsed) => !collapsed)}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div
+            className={classnames("navbar-collapse", { collapse: collapsed })}
+            id="navbar"
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
+              <Dropdown as={NavItem}>
+                <Dropdown.Toggle
+                  as={NavLink}
+                  className={classnames(styles["nav-link"], {
+                    [`fw-bold ${styles.active}`]: useIsActive("/gallery"),
+                  })}
+                >
+                  Catalog
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <NavDropdownItem href="/gallery/questions">
+                    Questions
+                  </NavDropdownItem>
+                  <NavDropdownItem href="/gallery/assessments">
+                    Assessments
+                  </NavDropdownItem>
+                  <NavDropdownItem href="/gallery/courses">
+                    Courses
+                  </NavDropdownItem>
+                  <NavDropdownItem href="/oer">OER</NavDropdownItem>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown as={NavItem}>
+                <Dropdown.Toggle
+                  as={NavLink}
+                  className={classnames(styles["nav-link"], {
+                    [`fw-bold ${styles.active}`]: useIsActive("/about"),
+                  })}
+                >
+                  About
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <NavDropdownItem href="/about">About Us</NavDropdownItem>
+                  <NavDropdownItem href="/research">Research</NavDropdownItem>
+                </Dropdown.Menu>
+              </Dropdown>
+              <li className="nav-item">
+                <RouterNavLink href="/contact">Support</RouterNavLink>
+              </li>
+              <li className="nav-item">
+                <RouterNavLink href="/pricing">Pricing</RouterNavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <RequestCourseModal
+        show={showRequestCourseModal}
+        onHide={() => setShowRequestCourseModal(false)}
+      />
+    </React.Fragment>
   );
 };
