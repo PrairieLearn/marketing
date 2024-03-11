9fdb06df9411b473d0117aa6a5c790eddc875631
Mariana Silva <mfsilva@illinois.edu>
2024-03-11 10:34:47 -0500

    another attempt for value prop section

--- src/pages/products/testing-center/index.tsx
+++ src/pages/products/testing-center/index.tsx
@@ -12,6 +12,22 @@ import reducedDistractionImage from "../../../lib/images/reduced-distraction.png
 
 import styles from "./index.module.scss";
 
+interface ValuePropCardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const ValuePropCard: React.FC<ValuePropCardProps> = ({ title, children }) => {
+  return (
+    <article className={classnames("card overflow-hidden")}>
+      <div className="card-body">
+        <h5 className="card-title h5">{title}</h5>
+        {children}
+      </div>
+    </article>
+  );
+};
+
 export default function TestingCenter() {
   return (
     <React.Fragment>
@@ -38,7 +54,7 @@ export default function TestingCenter() {
             </div>
             <div className="col-md-6 order-1 pt-4">
               <div className="row">
-                <Heading>What is a Testing Center?</Heading>
+                <Heading>What is a Testing Center</Heading>
               </div>
               <p>
                 A testing center is a proctored computer lab where students can
@@ -49,25 +65,49 @@ export default function TestingCenter() {
         </div>
       </div>
 
-      <div className={classnames("container-fluid py-4")}>
+      <div className={classnames("container-fluid my-4")}>
         <div className="container-md">
           <div className="row">
-            <div className="col-md-6 order-1 pt-4">
-            <div className="row">
-                <Heading>The most secure exams</Heading>
-              </div>
-              <p>
-                Firewall and managed computers, hired professional proctors, screen filters, cameras
-              </p>
-            </div>
-            <div className="col-md-6 order-2 pt-4">
-              <div className="row">
-                <Heading>Reliable student verification</Heading>
-              </div>
-              <p>
-              (_who_ is taking the exam, are they taking it _alone_)
-              </p>
-            </div>
+            <h4 className={classnames("py-3 ", styles.color, styles.center)}>
+              Benefits from running exams at a Testing Center
+            </h4>
+          </div>
+          <div className={styles.grid}>
+            <ValuePropCard title="The most secure exams">
+              Maximize test integrity with managed computers, lab firewall, and
+              vigilant proctoring. Students access only approved resources,
+              while cheating risks are minimized through computer screens, video
+              cameras and professional oversight.
+            </ValuePropCard>
+            <ValuePropCard title="Reliable check-in process">
+              Enhance security measures by verifying students&apos; identities
+              at the lab entrance through their student ID cards. In addition,
+              all personal belongings, including phones, are securely stored
+              before entering the testing environment.
+            </ValuePropCard>
+            <ValuePropCard title="Flexible and efficient scheduling">
+              Offer exams asynchronously over 2-3 days, optimizing space
+              utilization in smaller computer labs for larger classes. Eliminate
+              the need for instructors to manage make-up or conflict exams,
+              streamlining the testing process for both students and faculty.
+            </ValuePropCard>
+            <ValuePropCard title="Student-centric exam scheduling">
+              Empower students with the flexibility to schedule exams at their
+              preferred times within the exam period. Enable easy reservation
+              modifications and cancellations up to the selected exam time,
+              providing convenience and autonomy to students.
+            </ValuePropCard>
+            <ValuePropCard title="Seamless Exam Overrides">
+              Empower instructors with the capability to effortlessly grant
+              overrides for students facing unexpected circumstances, such as
+              illness or unavoidable absences during the exam period.
+            </ValuePropCard>
+            <ValuePropCard title="Authentic testing">
+              Create an authentic testing environment by providing students with
+              access to computers equipped with compilers, debuggers, and
+              graphical software, ensuring a comprehensive assessment of their
+              skills and knowledge.
+            </ValuePropCard>
           </div>
         </div>
       </div>
@@ -112,29 +152,6 @@ export default function TestingCenter() {
         </div>
       </div>
 
-
-      <div className={classnames("container-fluid py-4")}>
-        <div className="container-md">
-          <div className="row">
-            <div className="col-md-6 order-1 pt-4">
-            <div className="row">
-                <Heading>Asynchronous exams</Heading>
-              </div>
-              <p>
-                no more conflict
-              </p>
-            </div>
-            <div className="col-md-6 order-2 pt-4">
-              <div className="row">
-                <Heading>Easy to schedule</Heading>
-              </div>
-              <p>
-              self-schedule, re-schedule
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
       <BannerCTA
         title="Need more information?"
         subtitle="Are you interested in building a testing center? We can help!"
