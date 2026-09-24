dd256d4de12d0c63b78d2f38df208a766453771d
Matthew West <mwest@illinois.edu>
2026-09-24 17:47:42 -0500

    Move community support access into PrairieLearn

--- next.config.ts
+++ next.config.ts
@@ -25,8 +25,12 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/slack",
-        destination:
-          "https://join.slack.com/t/prairielearn/shared_invite/zt-13kx0hg6v-uuC3kyt_3iBxjSpyhCbYVw",
+        destination: "/support",
+        permanent: true,
+      },
+      {
+        source: "/support/virtual-meetings",
+        destination: "/support",
         permanent: true,
       },
       {
